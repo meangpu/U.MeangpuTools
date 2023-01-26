2fd9dc6030175e855c60e6773eb6c303a4e07117
meangpu <51825872+meangpu@users.noreply.github.com>
2023-01-27 03:55:30 +0700

    add auto update manifest

--- Editor/ToolsMenu.cs
+++ Editor/ToolsMenu.cs
@@ -1,3 +1,6 @@
+using System.Net.Mime;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Http;
 using System.IO;
 using static System.IO.Directory;
 using static System.IO.Path;
@@ -27,6 +30,34 @@ namespace meangpu
 
             }
         }
+
+        [MenuItem("MeangpuTools/Setup/Load New Manifest")]
+        static async void LoadNewManifest()
+        {
+            // https://gist.githubusercontent.com/meangpu/25cefd54ac38510850b5174a6026b64e/raw/
+            var url = GetGistUrl("25cefd54ac38510850b5174a6026b64e");
+            var contents = await GetContents(url);
+            ReplacePackageFile(contents);
+        }
+
+        static string GetGistUrl(string id, string user = "meangpu") => $"https://gist.githubusercontent.com/{user}/{id}/raw/";
+
+        static async Task<string> GetContents(string url)
+        {
+            using var client = new HttpClient();
+            var respond = await client.GetAsync(url);
+            var content = await respond.Content.ReadAsStringAsync();
+            return content;
+        }
+
+        static void ReplacePackageFile(string contents)
+        {
+            var existing = Path.Combine(Application.dataPath, "../Packages/manifest.json");
+            File.WriteAllText(existing, contents);
+            UnityEditor.PackageManager.Client.Resolve();
+        }
+
+
     }
 
 }
