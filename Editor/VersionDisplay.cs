c09d5ed06632eb8f5652cd12a14fff9dbbe5465c
meangpu <51825872+meangpu@users.noreply.github.com>
2023-04-22 00:06:49 +0700

    Update VersionDisplay.cs

--- Editor/VersionDisplay.cs
+++ Editor/VersionDisplay.cs
@@ -24,7 +24,8 @@ namespace meangpu
                 SetFirstTimeFormat();
             }
             TMP_Text textVersion = GetComponent<TMP_Text>();
-            string[] versionValue = Application.version.Split('-', 2, StringSplitOptions.None); // split in to 2 part
+            string[] versionValue = Application.version.Split('-', 2); // split in to 2 part
+
             textVersion.text = $"{_frontWord}{versionValue[0]}\n<size=20>{versionValue[1]}";
         }
 
