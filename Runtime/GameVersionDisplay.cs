d127ca86937344d83ba0c120afe885aa2dcbc38f
meangpu <51825872+meangpu@users.noreply.github.com>
2023-04-23 06:10:58 +0700

    Update GameVersionDisplay.cs

--- Runtime/GameVersionDisplay.cs
+++ Runtime/GameVersionDisplay.cs
@@ -1,7 +1,5 @@
 using UnityEngine;
-using UnityEditor;
 using TMPro;
-using System;
 
 [RequireComponent(typeof(TMP_Text))]
 [ExecuteInEditMode]
@@ -13,28 +11,16 @@ public class GameVersionDisplay : MonoBehaviour
 
     void MainLoop()
     {
-        if (!IsProjectFormatCorrect()) SetFirstTimeFormat();
-
         TMP_Text textVersion = GetComponent<TMP_Text>();
         string[] versionValue = Application.version.Split('-', 2); // split in to 2 part
+        if (versionValue.Length == 2)
+        {
+            textVersion.text = $"{_frontWord}{versionValue[0]}\n<size=20>{versionValue[1]}";
+        }
+        else
+        {
+            textVersion.text = $"<color=red>Game version format is wrong</color>";
+        }
 
-        textVersion.text = $"{_frontWord}{versionValue[0]}\n<size=20>{versionValue[1]}";
-    }
-
-    bool IsProjectFormatCorrect()
-    {
-        string[] currentVersionNumber = PlayerSettings.bundleVersion.Split('-');
-        return currentVersionNumber.Length >= 1 ? true : false;
-    }
-
-    public static void SetFirstTimeFormat()
-    {
-        Debug.Log("Perform first time version set");
-        string date = DateTime.Now.ToString("d-MMM-yyy");
-        string time = DateTime.Now.ToString("t");
-        string finalDateTime = $"{date} {time}";
-        PlayerSettings.bundleVersion = $"0.0-{finalDateTime}";
-        Debug.Log(PlayerSettings.bundleVersion);
-        UnityEditor.AssetDatabase.Refresh();
     }
 }
