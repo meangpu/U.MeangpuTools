ab24cc63cb49200272afb653e09ad4ccaf53739f
meangpu <51825872+meangpu@users.noreply.github.com>
2023-04-25 19:10:17 +0700

    Update KillAllChild.cs

--- Runtime/KillAllChild.cs
+++ Runtime/KillAllChild.cs
@@ -7,5 +7,15 @@ public class KillAllChild : MonoBehaviour
         for (int i = parentTransform.childCount; i > 0; --i)
             DestroyImmediate(parentTransform.GetChild(0).gameObject);
     }
+    public static void KillWithTag(Transform parentTransform, string tag)
+    {
+        // compare children of game object
+        for (var i = parentTransform.childCount - 1; i >= 0; i--)
+        {
+            // only destroy tagged object
+            if (parentTransform.GetChild(i).gameObject.CompareTag(tag))
+                DestroyImmediate(parentTransform.GetChild(i).gameObject);
+        }
+    }
 
 }
