9022e84efe65f7ea44fa3ad0dfa7f5d64da1964d
meangpu <51825872+meangpu@users.noreply.github.com>
2023-04-30 03:31:58 +0700

    delete read json

---
+++ Editor/PackageImport/MePackageData.cs
@@ -0,0 +1,9 @@
+[System.Serializable]
+public class MePackageData
+{
+    public string name;
+    public string version;
+    public string description;
+    public string url;
+    public bool isSelectAtStart;
+}
