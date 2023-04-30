4a66db0234c10c0db2a714f185fa945807f4969a
meangpu <51825872+meangpu@users.noreply.github.com>
2023-04-30 22:24:45 +0700

    move screenshot to editor

---
+++ Editor/ScreenShot/ScreenShot.cs
@@ -0,0 +1,29 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+public class Screenshot
+{
+    [MenuItem("Screenshot/Grab")]
+    public static void Grab()
+    {
+        // if not Assets/ it will save at project root
+        string imageName = $"Assets/Screenshot{GetCh()}{GetCh()}{GetCh()}_{Screen.width}x{Screen.height}.png";
+
+        ScreenCapture.CaptureScreenshot(imageName, 1);
+
+        Debug.Log($"<color=#4ec9b0>{imageName} was create!</color>");
+        SelectObjectOnEditor(imageName);
+
+    }
+
+    private static void SelectObjectOnEditor(string imageName)
+    {
+        AssetDatabase.Refresh();
+        Object obj = AssetDatabase.LoadAssetAtPath(imageName, typeof(Object));
+        Selection.activeObject = obj;
+    }
+
+    public static char GetCh() => (char)UnityEngine.Random.Range('A', 'Z');
+}
+#endif
