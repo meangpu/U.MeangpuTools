4b76346807bcd37c905e31bc203827b7d5fc5af0
meangpu <51825872+meangpu@users.noreply.github.com>
2023-05-03 18:19:30 +0700

    put example of so super create

---
+++ Runtime/SO/SOSuperCreation.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEditor;
+using EasyButtons;
+
+[CreateAssetMenu(fileName = "New SOSuperXrayCreator", menuName = "Meangpu/SOSuperXrayCreator")]
+public class SoSuperCreation : ScriptableObject
+{
+    public Sprite[] _xrayTutorial;
+    public Sprite[] _xrayImg;
+    public string[] _operationName;
+    // public XrayLegLocation[] _legLocation;
+    // public LegPose[] _legPose;
+
+    // [Button]
+    // void CreateSOobject()
+    // {
+    //     if (_xrayTutorial.Length != _xrayImg.Length ||
+    //     _xrayTutorial.Length != _operationName.Length ||
+    //     _xrayTutorial.Length != _legLocation.Length ||
+    //     _xrayTutorial.Length != _legPose.Length)
+    //     {
+    //         Debug.Log($"<color=red>Image is not equal</color>");
+    //         return;
+    //     }
+
+    //     for (var i = 0; i < _xrayTutorial.Length; i++)
+    //     {
+    //         SOXrayOperation nowXrayOperation = ScriptableObject.CreateInstance<SOXrayOperation>();
+    //         string path = $"Assets/_Project/__Scripts/Xray/SO/OperationData/{i + 1}.asset";
+
+    //         nowXrayOperation.TutorialImg = _xrayTutorial[i];
+    //         nowXrayOperation.XrayOutImage = _xrayImg[i];
+    //         nowXrayOperation.XrayName = _operationName[i];
+    //         nowXrayOperation.LegLocation = _legLocation[i];
+    //         nowXrayOperation.LegPose = _legPose[i];
+
+    //         nowXrayOperation.name = (i + 1).ToString();
+
+    //         AssetDatabase.CreateAsset(nowXrayOperation, path);
+    //         AssetDatabase.SaveAssets();
+    //         Debug.Log($"create {i + 1} obj at {path}!");
+    //         AssetDatabase.Refresh();
+    //         EditorUtility.FocusProjectWindow();
+    //         Selection.activeObject = nowXrayOperation;
+    //     }
+
+
+
+    // }
+}
