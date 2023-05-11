6ec57f81fb1faff152c5e770c71f523a26f45589
meangpu <51825872+meangpu@users.noreply.github.com>
2023-05-12 00:34:48 +0700

    add drag and drop

---
+++ Runtime/Util/DragAndDrop.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    Vector3 _mousePos;
+    Camera _mainCam;
+
+    private void Start() => _mainCam = Camera.main;
+
+    Vector3 GetMousePos() => _mainCam.WorldToScreenPoint(transform.position);
+
+    void OnMouseDown()
+    {
+        _mousePos = Input.mousePosition - GetMousePos();
+    }
+
+    void OnMouseDrag()
+    {
+        transform.position = _mainCam.ScreenToWorldPoint(Input.mousePosition - _mousePos);
+    }
+
+
+}
