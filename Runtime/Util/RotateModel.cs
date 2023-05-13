d9f472ef6bb8b36e58fd9c1632adb9014346f26a
meangpu <51825872+meangpu@users.noreply.github.com>
2023-05-14 00:31:02 +0700

    Update RotateModel.cs

--- Runtime/Util/RotateModel.cs
+++ Runtime/Util/RotateModel.cs
@@ -4,8 +4,8 @@ public class RotateModel : MonoBehaviour
     [SerializeField] Vector3 _rotSpeed = new Vector3(0, 1, 0);
     Vector3 _nowRotSpeed;
 
-    void RotateStop() => _nowRotSpeed = Vector3.zero;
-    void RotateStart() => _nowRotSpeed = _rotSpeed;
+    public void RotateStop() => _nowRotSpeed = Vector3.zero;
+    public void RotateStart() => _nowRotSpeed = _rotSpeed;
 
     private void Start() => _nowRotSpeed = _rotSpeed;
 
