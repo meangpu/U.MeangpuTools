2e639bc64e23ed52c9facfc6d92d9dff685ebd7e
meangpu <51825872+meangpu@users.noreply.github.com>
2023-06-08 08:09:31 +0700

    Update DragAndDrop.cs

--- Runtime/Util/DragAndDrop.cs
+++ Runtime/Util/DragAndDrop.cs
@@ -1,23 +1,24 @@
 using UnityEngine;
 
-public class DragAndDrop : MonoBehaviour
+namespace MeangpuTools
 {
-    Vector3 _mousePos;
-    Camera _mainCam;
-
-    private void Start() => _mainCam = Camera.main;
-
-    Vector3 GetMousePos() => _mainCam.WorldToScreenPoint(transform.position);
-
-    void OnMouseDown()
+    public class DragAndDrop : MonoBehaviour
     {
-        _mousePos = Input.mousePosition - GetMousePos();
+        Vector3 _mousePos;
+        Camera _mainCam;
+
+        private void Start() => _mainCam = Camera.main;
+
+        Vector3 GetMousePos() => _mainCam.WorldToScreenPoint(transform.position);
+
+        void OnMouseDown()
+        {
+            _mousePos = Input.mousePosition - GetMousePos();
+        }
+
+        void OnMouseDrag()
+        {
+            transform.position = _mainCam.ScreenToWorldPoint(Input.mousePosition - _mousePos);
+        }
     }
-
-    void OnMouseDrag()
-    {
-        transform.position = _mainCam.ScreenToWorldPoint(Input.mousePosition - _mousePos);
-    }
-
-
 }
