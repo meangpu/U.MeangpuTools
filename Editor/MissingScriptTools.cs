55dba72b278e38860ee12ac3210caec17033ad46
meangpu <51825872+meangpu@users.noreply.github.com>
2023-06-13 03:25:37 +0700

    add missing scpt destroyer

--- Editor/MissingScriptTools.cs
+++ Editor/MissingScriptTools.cs
@@ -1,12 +1,45 @@
 #if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
+#endif
 
 namespace MeangpuTools
 {
-#endif
-
-    public class MissingScriptTools
+    public static class MissingScriptTools
     {
+        const string _commandName = "MeangpuTools/MissingScpt/";
+
+        [MenuItem(_commandName + "Find")]
+        static void FindMissingScript()
+        {
+            foreach (GameObject gameObject in GameObject.FindObjectsOfType<GameObject>(true))
+            {
+                foreach (Component component in gameObject.GetComponentsInChildren<Component>())
+                {
+                    if (component == null)
+                    {
+                        Debug.Log($"<color=red>MissingScpt: {gameObject.name}</color>", gameObject);
+                        break;
+                    }
+                }
+            }
+        }
+
+        [MenuItem(_commandName + "Delete")]
+        static void DeleteMissingScript()
+        {
+            foreach (GameObject gameObject in GameObject.FindObjectsOfType<GameObject>(true))
+            {
+                foreach (Component component in gameObject.GetComponentsInChildren<Component>())
+                {
+                    if (component == null)
+                    {
+                        GameObjectUtility.RemoveMonoBehavioursWithMissingScript(gameObject);
+                        Debug.Log($"<color=red>DELETE:{gameObject.name}</color>", gameObject);
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
