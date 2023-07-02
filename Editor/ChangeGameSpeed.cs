a57502dc8635b5246df507a32d012cd9736f43b3
meangpu <51825872+meangpu@users.noreply.github.com>
2023-07-02 13:03:33 +0700

    Update ChangeGameSpeed.cs

--- Editor/ChangeGameSpeed.cs
+++ Editor/ChangeGameSpeed.cs
@@ -1,39 +1,49 @@
 using UnityEditor;
 using UnityEngine;
 
-public class ChangeGameSpeed : EditorWindow
+namespace Meangpu
 {
-
-    public float GameSpeed;
-
-    [MenuItem("MeangpuTools/GameSpeed")]
-    public static void ShowWindow()
+    public class ChangeGameSpeed : EditorWindow
     {
-        GetWindow<ChangeGameSpeed>("Set Game Speed");
+        public float GameSpeed;
+        public int GameFramerate = 1;
+
+        [MenuItem("MeangpuTools/GameSpeed")]
+        public static void ShowWindow()
+        {
+            GetWindow<ChangeGameSpeed>("Set Game Speed");
+        }
+
+        private void OnGUI()
+        {
+            // obj example
+            // _targetPref = EditorGUILayout.ObjectField("TargetPrefab", _targetPref, typeof(Transform), true) as Transform;
+
+            GameSpeed = EditorGUILayout.FloatField("GameSpeed", GameSpeed);
+
+            EditorGUILayout.LabelField("TIME SETTING", EditorStyles.boldLabel);
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("x0.1")) Time.timeScale = 0.1f;
+            if (GUILayout.Button("x0.5")) Time.timeScale = 0.5f;
+            if (GUILayout.Button("x1")) Time.timeScale = 1;
+            if (GUILayout.Button("x2")) Time.timeScale = 2;
+            if (GUILayout.Button("x5")) Time.timeScale = 5;
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("x10")) Time.timeScale = 10;
+            if (GUILayout.Button("x20")) Time.timeScale = 20;
+            if (GUILayout.Button("InputFloatBoxSpeed")) Time.timeScale = GameSpeed;
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.LabelField("Framerate", EditorStyles.boldLabel);
+            GameFramerate = EditorGUILayout.IntField("GameFramerate", GameFramerate);
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("5")) Application.targetFrameRate = 5;
+            if (GUILayout.Button("10")) Application.targetFrameRate = 10;
+            if (GUILayout.Button("30")) Application.targetFrameRate = 30;
+            if (GUILayout.Button("InputBox")) Application.targetFrameRate = GameFramerate;
+            EditorGUILayout.EndHorizontal();
+        }
     }
-
-    private void OnGUI()
-    {
-        // obj example
-        // _targetPref = EditorGUILayout.ObjectField("TargetPrefab", _targetPref, typeof(Transform), true) as Transform;
-
-        GameSpeed = EditorGUILayout.FloatField("layer1", GameSpeed);
-
-        EditorGUILayout.LabelField("TIME SETTING", EditorStyles.boldLabel);
-        EditorGUILayout.BeginHorizontal();
-        if (GUILayout.Button("x0.1")) Time.timeScale = 0.1f;
-        if (GUILayout.Button("x0.5")) Time.timeScale = 0.5f;
-        if (GUILayout.Button("x1")) Time.timeScale = 1;
-        if (GUILayout.Button("x2")) Time.timeScale = 2;
-        if (GUILayout.Button("x5")) Time.timeScale = 5;
-        EditorGUILayout.EndHorizontal();
-
-        EditorGUILayout.BeginHorizontal();
-        if (GUILayout.Button("x10")) Time.timeScale = 10;
-        if (GUILayout.Button("x20")) Time.timeScale = 20;
-        if (GUILayout.Button("InputFloatBoxSpeed")) Time.timeScale = GameSpeed;
-        EditorGUILayout.EndHorizontal();
-
-    }
-
 }
