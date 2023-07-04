71e46ebcdc930cb6207c5cffeb4796f8b9d36828
meangpu <51825872+meangpu@users.noreply.github.com>
2023-07-05 03:53:22 +0700

    Update TransformSetTargetObjectHeight.cs

--- Runtime/Util/TransformSetTargetObjectHeight.cs
+++ Runtime/Util/TransformSetTargetObjectHeight.cs
@@ -6,7 +6,8 @@ namespace Meangpu.Util
     {
         [SerializeField] Transform _targetTransform;
         // horizontal mean it vertical bar but it slider left right that set object height
-        [SerializeField] bool _sliderIsHorizontal;
+        [SerializeField] bool _sliderIsUsingX;
+        [SerializeField] bool _sliderIsUsingZ;
         [SerializeField] float _moveMultiplier = 1;
         Vector3 _startPos;
 
@@ -14,14 +15,19 @@ namespace Meangpu.Util
 
         float GetOffsetY() => -(_startPos.y - transform.localPosition.y);
         float GetOffsetX() => -(_startPos.x - transform.localPosition.x);
+        float GetOffsetZ() => -(_startPos.z - transform.localPosition.z);
 
         void Update() => UpdatePinPosToSetTargetObjectHeight();
 
         void UpdatePinPosToSetTargetObjectHeight()
         {
-            if (_sliderIsHorizontal)
+            if (_sliderIsUsingX)
             {
                 _targetTransform.position = new Vector3(_targetTransform.position.x, GetOffsetX() * _moveMultiplier, _targetTransform.position.z);
+            }
+            else if (_sliderIsUsingZ)
+            {
+                _targetTransform.position = new Vector3(_targetTransform.position.x, GetOffsetZ() * _moveMultiplier, _targetTransform.position.z);
             }
             else
             {
