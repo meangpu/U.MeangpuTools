d0250fadd8f8bc51a576239f36d04321b7038943
meangpu <51825872+meangpu@users.noreply.github.com>
2023-07-06 15:21:25 +0700

    Interface

---
+++ Runtime/Interface/ICanDisplayData.cs
@@ -0,0 +1,8 @@
+namespace Meangpu.Interface
+{
+    public interface ICanDisplayData
+    {
+        // have setup data first then display data with this method
+        public void DisplayData();
+    }
+}
