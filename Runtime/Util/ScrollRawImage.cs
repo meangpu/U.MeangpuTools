a232f1dadb9f33e404b2668d06c5fbefb06d0df8
meangpu <51825872+meangpu@users.noreply.github.com>
2023-07-17 02:25:15 +0700

    update scroll raw img

---
+++ Runtime/Util/ScrollRawImage.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Meangpu.Util
+{
+    // from Tarodev [Scrolling Background in 90 seconds - Unity Tutorial - YouTube](https://www.youtube.com/watch?v=-6H-uYh80vc) he is the best guy ever! 
+    public class ScrollRawImage : MonoBehaviour
+    {
+        [Tooltip("image need wrap mode to be repeat")]
+        [SerializeField] RawImage _img;
+        [SerializeField] Vector2 _scrollDir;
+
+        private void FixedUpdate()
+        {
+            _img.uvRect = new Rect(_img.uvRect.position + (_scrollDir * Time.fixedDeltaTime), _img.uvRect.size);
+        }
+    }
+}
