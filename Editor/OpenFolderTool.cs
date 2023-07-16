098b20fe1d04b94580ca56099df2621f31c3506f
meangpu <51825872+meangpu@users.noreply.github.com>
2023-07-17 03:30:07 +0700

    shift click open

---
+++ Editor/OpenFolderTool.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public class OpenFolderTool
+{
+    [OnOpenAsset]
+    public static bool OnOpenAsset(int instanceId)
+    {
+        Event e = Event.current;
+
+        if (e?.shift != true)
+            return false;
+
+        Object obj = EditorUtility.InstanceIDToObject(instanceId);
+        string path = AssetDatabase.GetAssetPath(obj);
+        if (AssetDatabase.IsValidFolder(path))
+        {
+            EditorUtility.RevealInFinder(path);
+        }
+        return true;
+    }
+}
