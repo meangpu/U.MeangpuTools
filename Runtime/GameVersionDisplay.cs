f968be3410b70f5bbc487b1affcf9002dff59fa9
meangpu <51825872+meangpu@users.noreply.github.com>
2023-07-22 21:18:08 +0700

    Update GameVersionDisplay.cs

--- Runtime/GameVersionDisplay.cs
+++ Runtime/GameVersionDisplay.cs
@@ -8,6 +8,7 @@ namespace Meangpu
     public class GameVersionDisplay : MonoBehaviour
     {
         [SerializeField] string _frontWord = "v.";
+        [SerializeField] float _lowerTextSize = 20;
 
         void Awake() => MainLoop();
 
@@ -15,7 +16,7 @@ namespace Meangpu
         {
             TMP_Text textVersion = GetComponent<TMP_Text>();
             string[] versionValue = Application.version.Split('-', 2); // split in to 2 part
-            if (versionValue.Length == 2) textVersion.text = $"{_frontWord}{versionValue[0]}\n<size=20>{versionValue[1]}";
+            if (versionValue.Length == 2) textVersion.text = $"{_frontWord}{versionValue[0]}\n<size={_lowerTextSize}>{versionValue[1]}";
             else textVersion.text = $"<color=red>Game version format is wrong</color>";
         }
     }
