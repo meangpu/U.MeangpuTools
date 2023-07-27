c6380d97b9f15b6deaf284ce14b7dea0055a2be1
meangpu <51825872+meangpu@users.noreply.github.com>
2023-07-28 01:07:39 +0700

    create delete by name

---
+++ Editor/DeleteByName.cs
@@ -0,0 +1,30 @@
+using UnityEditor;
+using UnityEngine;
+using System.Linq;
+
+namespace Meangpu
+{
+    public class DeleteByName : EditorWindow
+    {
+        public string targetObjectName;
+
+        [MenuItem("MeangpuTools/DeleteGameObjectByName")]
+        public static void ShowWindow() => GetWindow<DeleteByName>("DeleteGameObjectByName");
+
+        private void OnGUI()
+        {
+            targetObjectName = EditorGUILayout.TextField("TargetObjectName", targetObjectName);
+            if (GUILayout.Button("DeleteAllObjectByName")) DeleteObjectByName();
+        }
+
+        void DeleteObjectByName()
+        {
+            var objects = Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => obj.name == targetObjectName);
+            foreach (var item in objects)
+            {
+                Debug.Log($"Do Delete{item.name}");
+                DestroyImmediate(item);
+            }
+        }
+    }
+}
