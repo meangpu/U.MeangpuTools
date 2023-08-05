f2c8fc16b4fd873e66042dacfbd663b9bde2b89f
meangpu <51825872+meangpu@users.noreply.github.com>
2023-08-05 19:04:30 +0700

    Update Thinker.cs

--- Runtime/SO/Brain/Thinker.cs
+++ Runtime/SO/Brain/Thinker.cs
@@ -4,6 +4,7 @@ namespace Meangpu.Brain
 {
     public class Thinker : MonoBehaviour
     {
+        // can make it into List<Brain> then update brain by hp condition
         public Brain Brain;
         private void Update() => Brain.Think(this);
     }
