cb2f80d008384670ddc2b71b0cf43475156ce8ed
meangpu <51825872+meangpu@users.noreply.github.com>
2023-08-15 00:08:00 +0700

    better image preview
    the old system, will overwrite image value if select multiple image

--- Editor/DataType/SpritePropertyDrawer.cs
+++ Editor/DataType/SpritePropertyDrawer.cs
@@ -3,40 +3,67 @@ using UnityEditor;
 
 namespace Meangpu.Datatype
 {
-    //original code from https://pastebin.com/Y1R7eSA0 
+    // from this https://forum.unity.com/threads/drawing-a-sprite-in-editor-window.419199/
     [CustomPropertyDrawer(typeof(Sprite))]
     public class SpritePropertyDrawer : PropertyDrawer
     {
-        public override float GetPropertyHeight(SerializedProperty property, GUIContent labelN)
+        const float imageHeight = 100;
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
-            if (property.objectReferenceValue != null) return _texSize;
-            return base.GetPropertyHeight(property, labelN);
+            if (property.propertyType == SerializedPropertyType.ObjectReference &&
+                ((Sprite)property.objectReferenceValue) != null)
+            {
+                return EditorGUI.GetPropertyHeight(property, label, true) + imageHeight + 10;
+            }
+            return EditorGUI.GetPropertyHeight(property, label, true);
         }
 
-        private const float _texSize = 100;
-
-        public override void OnGUI(Rect position, SerializedProperty prop, GUIContent label)
+        static string GetPath(SerializedProperty property)
         {
-            EditorGUI.BeginProperty(position, label, prop);
+            string path = property.propertyPath;
+            int index = path.LastIndexOf(".");
+            return path[..(index + 1)];
+        }
 
-            if (prop.objectReferenceValue != null)
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            //Draw the normal property field
+            EditorGUI.PropertyField(position, property, label, true);
+            if (property.propertyType == SerializedPropertyType.ObjectReference)
             {
-                position.width = EditorGUIUtility.labelWidth;
-                GUI.Label(position, prop.displayName);
-
-                position.x += position.width;
-                position.width = _texSize;
-                position.height = _texSize;
-
-                prop.objectReferenceValue =
-                    EditorGUI.ObjectField(position, prop.objectReferenceValue, typeof(Sprite), false);
+                var sprite = property.objectReferenceValue as Sprite;
+                if (sprite != null)
+                {
+                    position.y += EditorGUI.GetPropertyHeight(property, label, true) + 5;
+                    position.height = imageHeight;
+                    // GUI.DrawTexture(position, sprite.texture, ScaleMode.ScaleToFit);
+                    DrawTexturePreview(position, sprite);
+                }
             }
-            else
-            {
-                EditorGUI.PropertyField(position, prop, true);
-            }
+        }
 
-            EditorGUI.EndProperty();
+        private void DrawTexturePreview(Rect position, Sprite sprite)
+        {
+            Vector2 fullSize = new(sprite.texture.width, sprite.texture.height);
+            Vector2 size = new(sprite.textureRect.width, sprite.textureRect.height);
+
+            Rect coords = sprite.textureRect;
+            coords.x /= fullSize.x;
+            coords.width /= fullSize.x;
+            coords.y /= fullSize.y;
+            coords.height /= fullSize.y;
+
+            Vector2 ratio;
+            ratio.x = position.width / size.x;
+            ratio.y = position.height / size.y;
+            float minRatio = Mathf.Min(ratio.x, ratio.y);
+
+            Vector2 center = position.center;
+            position.width = size.x * minRatio;
+            position.height = size.y * minRatio;
+            position.center = center;
+
+            GUI.DrawTextureWithTexCoords(position, sprite.texture, coords);
         }
     }
 }
