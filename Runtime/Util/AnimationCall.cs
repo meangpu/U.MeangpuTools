948144ccfc87050c9fe3c2978322b2b6fe2b4336
meangpu <51825872+meangpu@users.noreply.github.com>
2023-09-18 05:11:23 +0700

    update animation call

--- Runtime/Util/AnimationCall.cs
+++ Runtime/Util/AnimationCall.cs
@@ -5,11 +5,14 @@ namespace Meangpu.Util
     public class AnimationCall : MonoBehaviour
     {
         protected string _nowState;
+        [SerializeField] protected Animator _animator;
+        [SerializeField] protected float _crossFadeTime = .5f;
 
-        public virtual void ChangeAnimationState(string newState, Animator animator)
+        public virtual void ChangeAnimationState(string newState)
         {
             if (_nowState == newState) return;
-            animator.Play(newState);
+            _animator.Play(newState);
+            _animator.CrossFade(newState, _crossFadeTime);
             _nowState = newState;
         }
     }
