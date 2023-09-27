ae2fb692ab32355d4929426282e22e0f214974ac
meangpu <51825872+meangpu@users.noreply.github.com>
2023-09-28 04:52:33 +0700

    Update SetTmpText.cs

--- Runtime/Util/SetTmpText.cs
+++ Runtime/Util/SetTmpText.cs
@@ -6,6 +6,8 @@ namespace Meangpu.Util
     public class SetTmpText : MonoBehaviour
     {
         [SerializeField] TMP_Text _targetText;
+        public TMP_Text TargetText => _targetText;
+
         public void SetText(string word) => _targetText.SetText(word);
         public void SetTextColor(Color newColor) => _targetText.color = newColor;
     }
