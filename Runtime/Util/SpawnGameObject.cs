9a719dbdceef1c4664bb41e63a624b77aff19a46
meangpu <51825872+meangpu@users.noreply.github.com>
2023-10-26 10:34:42 +0700

    add spawn obj by list

---
+++ Runtime/Util/SpawnGameObject.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace Meangpu.Util
+{
+    public class SpawnGameObject : SpawnListObject<ScriptableObject>
+    {
+    }
+}
---
+++ Runtime/Util/SpawnListObject.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using EasyButtons;
+using UnityEngine;
+
+namespace Meangpu.Util
+{
+    public abstract class SpawnListObject<T> : MonoBehaviour where T : Object
+    {
+        [SerializeField] List<T> _objectToSpawn;
+        [SerializeField] Transform _parentTrans;
+
+        [Button]
+        protected virtual void EditorSpawnEmptyObj()
+        {
+            if (_parentTrans == null) _parentTrans = gameObject.transform;
+            KillAllChild.KillAllChildInTransform(_parentTrans);
+            foreach (T obj in _objectToSpawn)
+            {
+                GameObject nowObj = new(obj.name);
+                nowObj.transform.SetParent(_parentTrans);
+            }
+        }
+    }
+}
