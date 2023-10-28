162c0e3cd7475434ab0660489a7d43d76508c7ac
meangpu <51825872+meangpu@users.noreply.github.com>
2023-10-28 10:27:50 +0700

    add debug optional

--- Runtime/SO/MeEvent/Listener/BaseGameEventListener.cs
+++ Runtime/SO/MeEvent/Listener/BaseGameEventListener.cs
@@ -8,6 +8,7 @@ namespace Meangpu.SOEvent
         [SerializeField] E _gameEvent;
         public E GameEvent { get { return _gameEvent; } set { _gameEvent = value; } }
         [SerializeField] UER _unityEventRespond;
+        [SerializeField] bool _doLogData;
 
         void OnEnable()
         {
@@ -23,6 +24,12 @@ namespace Meangpu.SOEvent
         public void OnEventRaised(T data)
         {
             if (_unityEventRespond != null) _unityEventRespond?.Invoke(data);
+#if UNITY_EDITOR
+            if (_doLogData)
+            {
+                Debug.Log($"SO:{_gameEvent}\ndata:{data}", _gameEvent);
+            }
+#endif
         }
     }
 }
