02a57e8908aba648d6a2e6591f156bfaf1de542b
meangpu <51825872+meangpu@users.noreply.github.com>
2023-10-28 11:03:43 +0700

    add event raiser for debug

---
+++ Runtime/SO/MeEvent/Event/VoidEventRaiser.cs
@@ -0,0 +1,11 @@
+using EasyButtons;
+using UnityEngine;
+
+namespace Meangpu.SOEvent
+{
+    public class VoidEventRaiser : MonoBehaviour
+    {
+        [SerializeField] SOVoidEvent _Data;
+        [Button] void RaiseEventData() => _Data?.Raise();
+    }
+}
