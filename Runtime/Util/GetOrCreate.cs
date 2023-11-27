c3e5218846ea434c67a288bedd128b42763e9776
meangpu <51825872+meangpu@users.noreply.github.com>
2023-11-27 15:54:22 +0700

    Update GetOrCreate.cs

--- Runtime/Util/GetOrCreate.cs
+++ Runtime/Util/GetOrCreate.cs
@@ -2,12 +2,18 @@ using UnityEngine;
 
 namespace Meangpu.Util
 {
-    public class GetOrCreate : MonoBehaviour
+    public static class GetOrCreate
     {
         public static T GetCreateComponent<T>(Transform _transform) where T : Component
         {
             if (_transform.GetComponent<T>()) return _transform.GetComponent<T>();
             return _transform.gameObject.AddComponent<T>();
+        }
+
+        public static T GetCreateComponent<T>(GameObject gameObject) where T : Component
+        {
+            if (gameObject.GetComponent<T>()) return gameObject.GetComponent<T>();
+            return gameObject.AddComponent<T>();
         }
         // ======= example =======
         // public BoxCollider _box;
