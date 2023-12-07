5606ba8d3debfdae9bb460deda355ef8015653a4
meangpu <51825872+meangpu@users.noreply.github.com>
2023-12-07 17:54:55 +0700

    Update ArrowPointToTarget.cs

--- Runtime/Util/ArrowPointToTarget.cs
+++ Runtime/Util/ArrowPointToTarget.cs
@@ -6,10 +6,16 @@ namespace Meangpu.Util
     {
         [SerializeField] Transform _target;
         [SerializeField] float _rotSpeed = 10f;
+        [SerializeField] Vector3 _allowRotationAxis = Vector3.one;
+        Vector3 _tempRot;
 
         private void Update()
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_target.position - transform.position), _rotSpeed * Time.deltaTime);
+
+            _tempRot = transform.localEulerAngles;
+            _tempRot = Vector3.Scale(_tempRot, _allowRotationAxis);
+            transform.localEulerAngles = _tempRot;
         }
     }
 }
