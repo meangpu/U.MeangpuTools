47e2667cbbb3a489f1d218fa70986486df626948
meangpu <51825872+meangpu@users.noreply.github.com>
2023-12-07 17:09:29 +0700

    Update SetActiveBlink.cs

--- Runtime/Util/SetActiveBlink.cs
+++ Runtime/Util/SetActiveBlink.cs
@@ -1,23 +1,71 @@
+using EasyButtons;
 using UnityEngine;
+using System.Collections;
 
 namespace Meangpu.Util
 {
     public class SetActiveBlink : MonoBehaviour
     {
         [SerializeField] GameObject _target;
+
+        [Header("duration")]
+        [SerializeField] float _onDuration = .9f;
+        [SerializeField] float _offDuration = .15f;
+
         [Header("Start state")]
-        [SerializeField] bool _nowActiveState;
-        [Header("Start setting")]
+        [SerializeField] bool _objectActiveState = true;
         [SerializeField] bool _blinkOnStart;
 
+        IEnumerator _enumerator;
         bool _isBlinking;
 
         void OnDisable() => StopAllCoroutines();
 
-        void ToggleObjState()
+        public void SetObjectState(bool state)
         {
-            _nowActiveState = !_nowActiveState;
-            _target?.SetActive(_nowActiveState);
+            _objectActiveState = state;
+            _target?.SetActive(state);
+        }
+
+        private void Start()
+        {
+            SetObjectState(_objectActiveState);
+            if (_blinkOnStart) StartBlink();
+        }
+
+        [Button]
+        public void StartBlink()
+        {
+            StopAllCoroutines();
+            _enumerator = Blink();
+            _isBlinking = true;
+            StartCoroutine(_enumerator);
+        }
+
+        [Button]
+        public void StopBlink()
+        {
+            _isBlinking = false;
+            if (_enumerator != null) StopCoroutine(_enumerator);
+            _enumerator = null;
+        }
+
+        [Button]
+        public void ToggleObjState()
+        {
+            _objectActiveState = !_objectActiveState;
+            SetObjectState(_objectActiveState);
+        }
+
+        IEnumerator Blink()
+        {
+            while (_isBlinking)
+            {
+                ToggleObjState();
+                yield return new WaitForSecondsRealtime(_offDuration);
+                ToggleObjState();
+                yield return new WaitForSecondsRealtime(_onDuration);
+            }
         }
     }
 }
