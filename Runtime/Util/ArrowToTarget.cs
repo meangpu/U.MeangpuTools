f6e4fe7bd3d8d73b3ffa82292520415fc4dffa68
meangpu <51825872+meangpu@users.noreply.github.com>
2023-12-07 17:40:33 +0700

    stable arrow

---
+++ Runtime/Util/ArrowToTarget.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace Meangpu.Util
+{
+    public class ArrowToTarget : MonoBehaviour
+    {
+        [SerializeField] Transform _target;
+        [SerializeField] float _rotSpeed = 10f;
+
+        private void Update()
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_target.position - transform.position), _rotSpeed * Time.deltaTime);
+        }
+    }
+}
