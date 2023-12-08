2631bfe9d4063d7d49462cae27734dae8959d22b
meangpu <51825872+meangpu@users.noreply.github.com>
2023-12-09 02:25:50 +0700

    add event bool

---
+++ Runtime/Util/EventBoolToggle.cs
@@ -0,0 +1,21 @@
+using EasyButtons;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Meangpu.Util
+{
+    public class EventBoolToggle : MonoBehaviour
+    {
+        [SerializeField] UnityEvent _OnEvent;
+        [SerializeField] UnityEvent _OffEvent;
+        [SerializeField] bool _nowBool;
+
+        [Button]
+        public void ToggleBtn()
+        {
+            _nowBool = !_nowBool;
+            if (_nowBool) _OnEvent?.Invoke();
+            else _OffEvent?.Invoke();
+        }
+    }
+}
