f519ec58e12a86057218e7aa6867fe70f53e9342
meangpu <51825872+meangpu@users.noreply.github.com>
2023-12-08 13:42:56 +0700

    Update ScreenShot.cs

--- Editor/ScreenShot.cs
+++ Editor/ScreenShot.cs
@@ -1,48 +1,61 @@
 using UnityEditor;
 using UnityEngine;
-using Unity.EditorCoroutines.Editor;
-using System.Collections;
 using System.IO;
 
 namespace Meangpu
 {
     public static class Screenshot
     {
-        public static Object currentImage;
+        public const string ScreenShotPath = "Assets/_Project/Screenshot";
 
         [MenuItem("Screenshot/GrabCam")]
         public static void GrabCam()
         {
-            string imageName = $"{Application.dataPath}/_Project/Screenshot/{GetCh()}{GetCh()}{GetCh()}_{Screen.width}x{Screen.height}.png";
-            ScreenCapture.CaptureScreenshot(imageName, 1);
-            Debug.Log($"<color=#4ec9b0>{imageName} was create!</color>");
-            EditorCoroutineUtility.StartCoroutine(SelectObjectOnEditor(imageName), new());
+            string fileName = $"_Project/Screenshot/{GetCh()}{GetCh()}{GetCh()}_{Screen.width}x{Screen.height}.png";
+            string filePath = $"{Application.dataPath}/{fileName}";
+            ScreenCapture.CaptureScreenshot(filePath, 1);
+            Debug.Log($"<color=#4ec9b0>{filePath} was create!</color>");
+            OpenScreenShotFolder();
         }
 
-        private static IEnumerator SelectObjectOnEditor(string imageName)
+        private static void SelectObjectOnEditor(string imageName)
         {
-            yield return new EditorWaitForSeconds(1.2f);
             AssetDatabase.Refresh();
             EditorUtility.FocusProjectWindow();
-            currentImage = AssetDatabase.LoadAssetAtPath(imageName, typeof(Object));
-            Selection.activeObject = currentImage;
+            Object nowImage = AssetDatabase.LoadAssetAtPath<Object>(imageName);
+            PingAndSetActive(nowImage);
+            AssetDatabase.Refresh();
+        }
+
+        public static void PingAndSetActive(Object obj)
+        {
+            Selection.activeObject = obj;
+            EditorGUIUtility.PingObject(obj);
+        }
+
+        private static void OpenScreenShotFolder()
+        {
+            AssetDatabase.Refresh();
+            EditorUtility.FocusProjectWindow();
+            Object obj = AssetDatabase.LoadAssetAtPath<Object>(ScreenShotPath);
+            AssetDatabase.OpenAsset(obj);
+            PingAndSetActive(obj);
+            AssetDatabase.Refresh();
         }
 
         public static char GetCh() => (char)Random.Range('A', 'Z');
 
         [MenuItem("Screenshot/GrabSceneViewNoFixed")]
-        public static void GrabSceneView(int width = 0, int height = 0)
+        public static void GrabSceneView()
         {
             // [CAPTURE the Scene View Camera Image in Unity - YouTube](https://www.youtube.com/watch?v=vrolg5A8jYE)
-
-            string imageName = $"{Application.dataPath}/_Project/Screenshot/{GetCh()}{GetCh()}{GetCh()}_{Screen.width}x{Screen.height}.png";
             SceneView view = SceneView.lastActiveSceneView;
 
-            if (width == 0 || height == 0)
-            {
-                width = view.camera.pixelWidth;
-                height = view.camera.pixelHeight;
-            }
+            int width = view.camera.pixelWidth;
+            int height = view.camera.pixelHeight;
+
+            string fileName = $"_Project/Screenshot/{GetCh()}{GetCh()}{GetCh()}_{width}x{height}.png";
+            string filePath = $"{Application.dataPath}/{fileName}";
 
             Texture2D capture = new(width, height);
             view.camera.Render();
@@ -51,13 +64,10 @@ namespace Meangpu
             capture.ReadPixels(new Rect(0, 0, width, height), 0, 0);
             capture.Apply();
             byte[] bytes = capture.EncodeToPNG();
-            Debug.Log($"{Application.dataPath + "/" + imageName}");
-            File.WriteAllBytes(imageName, bytes);
-            AssetDatabase.Refresh();
+            Debug.Log($"<color=#4ec9b0>{filePath} was create!</color>");
+            File.WriteAllBytes(filePath, bytes);
+
+            SelectObjectOnEditor($"Assets/{fileName}");
         }
-        [MenuItem("Screenshot/GrabSceneView_720")]
-        public static void GrabSceneView720() => GrabSceneView(1280, 720);
-        [MenuItem("Screenshot/GrabSceneView_1080")]
-        public static void GrabSceneView1080() => GrabSceneView(1920, 1080);
     }
 }
