8cceadc52acdb8ab4ba976d4d6878026a6830f32
meangpu <51825872+meangpu@users.noreply.github.com>
2023-12-08 15:31:47 +0700

    stable

--- Runtime/SO/SOCreateScene.cs
+++ Runtime/SO/SOCreateScene.cs
@@ -8,17 +8,25 @@ namespace Meangpu
     [CreateAssetMenu(menuName = "Meangpu/SOCreateScene")]
     public class SOCreateScene : SOCreateSOTemplate<SOScene>
     {
-        [Button] void SetPathToSOScene() => targetCreatePath = "Assets/Resources/SOScene/";
+        [Button]
+        void SetPathToSOScene()
+        {
+            targetCreatePath = "Assets/Resources/SOScene/";
+            EditorUtility.SetDirty(this);
+            AssetDatabase.Refresh();
+        }
+
         [SerializeField] SceneAsset[] _scene;
 
         public override void CreateSO_ObjectList()
         {
             for (var i = 0; i < _scene.Length; i++)
             {
-                SOScene nowScene = ScriptableObject.CreateInstance<SOScene>();
+                SOScene nowScene = CreateInstance<SOScene>();
 
                 nowScene.SceneData = _scene[i];
                 nowScene.name = _scene[i].name;
+                nowScene.SCENE_ID = _scene[i].name;
 
                 string path = GetFullFilePath(_scene[i].name);
                 CreateAsset(nowScene, path);
