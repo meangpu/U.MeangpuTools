d2181cb94e9c80c7e4a525d1d70755f79613721e
meangpu <51825872+meangpu@users.noreply.github.com>
2023-12-21 16:28:00 +0700

    stable

--- Editor/ComponentAddon/ExtractComponentToChild.cs
+++ Editor/ComponentAddon/ExtractComponentToChild.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEditor;
 using UnityEditorInternal;
+using System.Collections.Generic;
 
 namespace Meangpu.Util
 {
@@ -13,28 +14,84 @@ namespace Meangpu.Util
         public static void ExtractMenuOption(MenuCommand command)
         {
             Component sourceComponent = command.context as Component;
+            ExtractComponent(sourceComponent);
+        }
 
+        public static void ExtractComponent(Component targetComponent)
+        {
             int undoGroupIndex = Undo.GetCurrentGroup();
             Undo.IncrementCurrentGroup();
+            GameObject gameObject = CreateParentObj(targetComponent);
+            Undo.RegisterCreatedObjectUndo(gameObject, "Created child object");
+            ExtractOperation(targetComponent, undoGroupIndex, gameObject);
+        }
 
-            GameObject gameObject = new(sourceComponent.GetType().Name);
-            gameObject.transform.SetParent(sourceComponent.transform);
+        public static void ExtractComponent(List<Component> targetComponent)
+        {
+            int undoGroupIndex = Undo.GetCurrentGroup();
+            Undo.IncrementCurrentGroup();
+            GameObject gameObject = CreateParentObj(targetComponent[0]);
+            Undo.RegisterCreatedObjectUndo(gameObject, "Created child object");
+            foreach (Component component in targetComponent) ExtractOperation(component, undoGroupIndex, gameObject);
+        }
+
+        public static void ExtractComponent(Component[] targetComponent)
+        {
+            int undoGroupIndex = Undo.GetCurrentGroup();
+            Undo.IncrementCurrentGroup();
+            GameObject gameObject = CreateParentObj(targetComponent[0]);
+            Undo.RegisterCreatedObjectUndo(gameObject, "Created child object");
+            foreach (Component component in targetComponent) ExtractOperation(component, undoGroupIndex, gameObject);
+        }
+
+        private static GameObject CreateParentObj(Component targetComponent)
+        {
+            GameObject gameObject = new(targetComponent.GetType().Name);
+            gameObject.transform.SetParent(targetComponent.transform);
             gameObject.transform.localScale = Vector3.one;
             gameObject.transform.localPosition = Vector3.zero;
             gameObject.transform.localRotation = Quaternion.identity;
+            return gameObject;
+        }
 
-            Undo.RegisterCreatedObjectUndo(gameObject, "Created child object");
-
-            if (!ComponentUtility.CopyComponent(sourceComponent) || !ComponentUtility.PasteComponentAsNew(gameObject))
+        private static void ExtractOperation(Component targetComponent, int undoGroupIndex, GameObject gameObject)
+        {
+            if (!ComponentUtility.CopyComponent(targetComponent) || !ComponentUtility.PasteComponentAsNew(gameObject))
             {
-                Debug.LogError("Cannot extract component", sourceComponent.gameObject);
+                Debug.LogError("Cannot extract component", targetComponent.gameObject);
                 Undo.CollapseUndoOperations(undoGroupIndex);
                 Undo.PerformUndo();
                 return;
             }
-
-            Undo.DestroyObjectImmediate(sourceComponent);
+            Undo.DestroyObjectImmediate(targetComponent);
             Undo.CollapseUndoOperations(undoGroupIndex);
         }
+
+        // public static void ExtractComponent(Component[] targetComponent)
+        // {
+        //     int undoGroupIndex = Undo.GetCurrentGroup();
+
+        //     Undo.IncrementCurrentGroup();
+        //     GameObject gameObject = new(targetComponent[0].GetType().Name);
+
+        //     gameObject.transform.SetParent(targetComponent[0].transform);
+        //     gameObject.transform.localScale = Vector3.one;
+        //     gameObject.transform.localPosition = Vector3.zero;
+        //     gameObject.transform.localRotation = Quaternion.identity;
+        //     Undo.RegisterCreatedObjectUndo(gameObject, "Created child object");
+
+        //     foreach (Component component in targetComponent)
+        //     {
+        //         if (!ComponentUtility.CopyComponent(component) || !ComponentUtility.PasteComponentAsNew(gameObject))
+        //         {
+        //             Debug.LogError("Cannot extract component", component.gameObject);
+        //             Undo.CollapseUndoOperations(undoGroupIndex);
+        //             Undo.PerformUndo();
+        //             return;
+        //         }
+        //         Undo.DestroyObjectImmediate(component);
+        //         Undo.CollapseUndoOperations(undoGroupIndex);
+        //     }
+        // }
     }
 }
