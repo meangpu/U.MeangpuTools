c66c73633c1aa7358ccb1737f5ab21dc67152410
meangpu <51825872+meangpu@users.noreply.github.com>
2024-01-04 06:08:00 +0700

    Update KillChildCollider.cs

--- Runtime/Util/KillChildCollider.cs
+++ Runtime/Util/KillChildCollider.cs
@@ -6,19 +6,20 @@ namespace Meangpu.Util
     public class KillChildCollider : MonoBehaviour
     {
         [SerializeField] Transform _parentTrans;
+        [SerializeField] bool _includeInactive = true;
 
         [Button]
         void KillAllChildCollider()
         {
             if (_parentTrans == null) _parentTrans = transform;
-            foreach (var c in _parentTrans.GetComponentsInChildren<Collider>(true)) DoDestroyComponent(c);
+            foreach (var c in _parentTrans.GetComponentsInChildren<Collider>(_includeInactive)) DoDestroyComponent(c);
         }
 
         [Button]
         void KillAllChildRigidbody()
         {
             if (_parentTrans == null) _parentTrans = transform;
-            foreach (var c in _parentTrans.GetComponentsInChildren<Rigidbody>(true)) DoDestroyComponent(c);
+            foreach (var c in _parentTrans.GetComponentsInChildren<Rigidbody>(_includeInactive)) DoDestroyComponent(c);
         }
 
         void DoDestroyComponent(Component c)
