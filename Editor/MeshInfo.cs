4e3e654ac136fde33f4122f15fcffc9cb4c8316a
meangpu <51825872+meangpu@users.noreply.github.com>
2024-01-07 10:07:06 +0700

    add count polygon

---
+++ Editor/MeshInfo.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEditor;
+
+// [How could I count my poly / tri? - Questions & Answers - Unity Discussions](https://discussions.unity.com/t/how-could-i-count-my-poly-tri/164226)
+namespace Meangpu
+{
+    public class MeshInfo : EditorWindow
+    {
+        private int vertexCount;
+        private int subMeshCount;
+        private int triangleCount;
+
+        [MenuItem("Tools/Mesh Info")]
+        static void Init()
+        {
+            // Get existing open window or if none, make a new one:
+            MeshInfo window = (MeshInfo)GetWindow(typeof(MeshInfo));
+            window.titleContent.text = "Mesh Info";
+        }
+        void OnSelectionChange() => Repaint();
+        void OnGUI()
+        {
+            if (Selection.activeGameObject && Selection.activeGameObject.GetComponent<MeshFilter>())
+            {
+                vertexCount = Selection.activeGameObject.GetComponent<MeshFilter>().sharedMesh.vertexCount;
+                triangleCount = Selection.activeGameObject.GetComponent<MeshFilter>().sharedMesh.triangles.Length / 3;
+                subMeshCount = Selection.activeGameObject.GetComponent<MeshFilter>().sharedMesh.subMeshCount;
+                EditorGUILayout.LabelField(Selection.activeGameObject.name);
+                EditorGUILayout.LabelField("Vertices: ", vertexCount.ToString());
+                EditorGUILayout.LabelField("Triangles: ", triangleCount.ToString());
+                EditorGUILayout.LabelField("SubMeshes: ", subMeshCount.ToString());
+            }
+        }
+    }
+}
