d8b38c061e8a405aa3c83549b4b702b70b2287ac
meangpu <51825872+meangpu@users.noreply.github.com>
2024-01-10 02:37:34 +0700

    add game version display

---
+++ Editor/ChangePlayMode.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace Meangpu
+{
+    public class ChangePlayMode : EditorWindow
+    {
+        [MenuItem("MeangpuTools/TogglePlayMode")]
+        public static void TOGGLE() => SetMode(!EditorSettings.enterPlayModeOptionsEnabled);
+        void Set__FALSE() => SetMode(false);
+        void Set__TRUE() => SetMode(true);
+        public static void SetMode(bool mode)
+        {
+            EditorSettings.enterPlayModeOptionsEnabled = mode;
+            UpdateStatusDisplay();
+        }
+        public static void UpdateStatusDisplay() => Debug.Log($"SetFast: {EditorSettings.enterPlayModeOptionsEnabled}");
+    }
+}
---
+++ Editor/DisplayGameVersionPanel.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace Meangpu
+{
+    public class DisplayGameVersionPanel : EditorWindow
+    {
+        private string versionName;
+        [MenuItem("MeangpuTools/GameVersionPanel")]
+        static void Init()
+        {
+            DisplayGameVersionPanel window = (DisplayGameVersionPanel)GetWindow(typeof(DisplayGameVersionPanel));
+            window.titleContent.text = "GameVersionPanel";
+        }
+        void OnGUI()
+        {
+            GUIStyle boldStyle = new()
+            {
+                fontSize = 15,
+                fontStyle = FontStyle.Bold,
+            };
+            boldStyle.normal.textColor = Color.white;
+            EditorGUILayout.LabelField(PlayerSettings.bundleVersion, boldStyle);
+        }
+    }
+}
