748beb0874f5027799f6dffd1ed732c57ebe9894
meangpu <51825872+meangpu@users.noreply.github.com>
2024-01-11 16:47:28 +0700

    add scale set

---
+++ Editor/ScaleSetterWindow.cs
@@ -0,0 +1,59 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Meangpu
+{
+    // learn make array from: [unity game engine - How to display & modify array in the Editor Window? - Stack Overflow](https://stackoverflow.com/questions/47753367/how-to-display-modify-array-in-the-editor-window)
+    public class ScaleSetterWindow : EditorWindow
+    {
+        // create for vr-movie scale text, because it ui with bad parent object, so need some helper class to fix font size
+        private Transform parentScaleRef;
+
+        public GameObject[] childObjects = { };
+        SerializedObject childSerializeObject;
+
+        [MenuItem("MeangpuTools/EditorUtil/ScaleSetter")]
+        public static void ShowWindow()
+        {
+            GetWindow<ScaleSetterWindow>("Scale Setter");
+        }
+
+        private void OnEnable()
+        {
+            ScriptableObject target = this;
+            childSerializeObject = new SerializedObject(target);
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label("Set Scale", EditorStyles.boldLabel);
+            parentScaleRef = EditorGUILayout.ObjectField("Parent Scale Ref", parentScaleRef, typeof(Transform), true) as Transform;
+            EditorGUILayout.Space();
+
+            childSerializeObject.Update();
+            SerializedProperty childProperty = childSerializeObject.FindProperty("childObjects");
+            EditorGUILayout.PropertyField(childProperty, true); // True means show children
+            childSerializeObject.ApplyModifiedProperties(); // Remember to apply modified properties
+
+            if (GUILayout.Button("Set Scale"))
+            {
+                SetChildScale();
+            }
+        }
+
+        private void SetChildScale()
+        {
+            if (parentScaleRef == null || childObjects == null)
+            {
+                Debug.LogError("Please set the parent scale reference and child objects.");
+                return;
+            }
+
+            foreach (GameObject childObject in childObjects)
+            {
+                childObject.transform.localScale = parentScaleRef.localScale;
+            }
+            Debug.Log("Child scales have been set to the parent scale.");
+        }
+    }
+}
