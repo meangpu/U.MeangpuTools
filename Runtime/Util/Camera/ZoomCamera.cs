bbeefa7b0f9309c64f9e7022c3a5b19b69ca68f2
meangpu <51825872+meangpu@users.noreply.github.com>
2024-01-17 00:40:43 +0700

    stable camera zoom

---
+++ Runtime/Util/Camera/ZoomCamera.cs
@@ -0,0 +1,71 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using VInspector;
+
+namespace Meangpu.Util
+{
+    public class CameraZoom : MonoBehaviour
+    {
+        [SerializeField] Transform _camTransform;
+        [SerializeField]
+        Vector2 _clampMinMax = new(-3, -.6f);
+
+        [SerializeField] bool _isUseMouseSlider;
+        [ShowIf("_isUseMouseSlider")]
+        [SerializeField] float _scrollZoomSpeed = 0;
+        [EndIf()]
+        [SerializeField] float _scrollSmoothDelta = 10f;
+        [SerializeField] float _startOffset = .3f;
+
+        [SerializeField] bool _isUsingSlider;
+        [ShowIf("_isUsingSlider")]
+        [SerializeField] Slider _sliderValue;
+
+        float _targetZPos;
+        Vector3 _targetPos = new();
+
+        void Start()
+        {
+            _targetZPos = _startOffset;
+
+            if (!_isUsingSlider || _sliderValue == null) return;
+            UpdateSliderValue();
+            _sliderValue.onValueChanged.AddListener(UpdateZValue);
+        }
+
+        private void UpdateSliderValue()
+        {
+            if (!_isUsingSlider || _sliderValue == null) return;
+            _sliderValue.minValue = _clampMinMax.x;
+            _sliderValue.maxValue = _clampMinMax.y;
+            _sliderValue.value = _startOffset;
+        }
+
+        public void UpdateZValue(float pos) => _targetZPos = pos;
+
+        void Update()
+        {
+            if (_isUseMouseSlider) ZoomInOut();
+            _targetPos.Set(_camTransform.position.x, _camTransform.position.y, _targetZPos);
+            _camTransform.position = Vector3.Lerp(_camTransform.position, _targetPos, _scrollSmoothDelta * Time.deltaTime);
+        }
+
+        private void ZoomInOut()
+        {
+            if (Input.mouseScrollDelta.y > 0)
+            {
+                _targetZPos += _scrollZoomSpeed;// mouse up
+            }
+            if (Input.mouseScrollDelta.y < 0)
+            {
+                _targetZPos -= _scrollZoomSpeed;// mouse down
+            }
+
+            _targetZPos = Mathf.Clamp(_targetZPos, _clampMinMax.x, _clampMinMax.y);
+
+            if (!_isUsingSlider || _sliderValue == null) return;
+            _sliderValue.value = _targetZPos;
+        }
+    }
+}
