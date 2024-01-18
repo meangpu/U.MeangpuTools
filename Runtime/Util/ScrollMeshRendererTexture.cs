be1f8c626ea574fb8b30fba7d8c26c55bc502aee
meangpu <51825872+meangpu@users.noreply.github.com>
2024-01-19 02:40:42 +0700

    Update ScrollMeshRendererTexture.cs

--- Runtime/Util/ScrollMeshRendererTexture.cs
+++ Runtime/Util/ScrollMeshRendererTexture.cs
@@ -9,5 +9,8 @@ namespace Meangpu.Util
 
         private void FixedUpdate() => _renderer.material.mainTextureOffset = new(Time.realtimeSinceStartup * _scrollSpeed.x, Time.realtimeSinceStartup * _scrollSpeed.y);
 
+        public void UpdateScrollSpeed(Vector2 newSpeed) => _scrollSpeed = newSpeed;
+        public void AddToScrollSpeed(Vector2 addedSpeed) => _scrollSpeed += addedSpeed;
+
     }
 }
