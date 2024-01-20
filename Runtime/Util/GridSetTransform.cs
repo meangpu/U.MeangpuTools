0d3facb10363bbac502f48a7a8badada4a5f07c8
meangpu <51825872+meangpu@users.noreply.github.com>
2024-01-21 01:27:16 +0700

    stable grid set transform

---
+++ Runtime/Util/GridSetTransform.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using VInspector;
+
+namespace Meangpu.Util
+{
+    public class GridSetTransform : MonoBehaviour
+    {
+        [SerializeField] Transform[] transformToSet;
+        [SerializeField] Vector3 _nowOffset = new();
+        int _gridXNow;
+        int _gridYNow;
+        [SerializeField] int _gridXCount = 3;
+        [SerializeField] float _gridXOffset = .12f;
+        [SerializeField] float _gridYOffset = 0.06f;
+
+        void Start() => SpawnAllUIBtn();
+
+        [Button]
+        public void SpawnAllUIBtn()
+        {
+            _gridXNow = 0;
+            _gridYNow = 0;
+
+            int YCount = transformToSet.Length / _gridXCount;
+            float removeX = _gridXOffset * (_gridXCount + 1) * 0.5f;
+            float removeY = _gridYOffset * YCount * 0.5f;
+
+            _nowOffset = Vector3.zero;
+            foreach (Transform item in transformToSet)
+            {
+                _gridXNow++;
+                _nowOffset = new Vector3((_gridXNow * _gridXOffset) - removeX, (_gridYNow * _gridYOffset) - removeY, 0);
+                item.localPosition = _nowOffset;
+                if (_gridXNow % _gridXCount == 0)
+                {
+                    _gridXNow = 0;
+                    _gridYNow++;
+                }
+            }
+        }
+    }
+}
