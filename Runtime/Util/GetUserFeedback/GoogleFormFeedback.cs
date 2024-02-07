bd516b0095a7558dd70d4bb76d1341ed480afba4
meangpu <51825872+meangpu@users.noreply.github.com>
2024-02-07 13:15:36 +0700

    Update GoogleFormFeedback.cs

--- Runtime/Util/GetUserFeedback/GoogleFormFeedback.cs
+++ Runtime/Util/GetUserFeedback/GoogleFormFeedback.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using System;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -5,6 +6,8 @@ using UnityEngine.UI;
 using System.Collections;
 using TMPro;
 using VInspector;
+using UnityEngine.Events;
+using Meangpu.SOEvent;
 
 namespace Meangpu.Util
 {
@@ -19,16 +22,19 @@ namespace Meangpu.Util
 
         [Header("User input choose ONE")]
         [SerializeField] TMP_InputField _InputFieldTmp;
-        [SerializeField] TMP_Text _formTextTmp;
+        [SerializeField] TMP_Text _textTmp;
         [SerializeField] InputField _inputField;
-        [SerializeField] Text _formText;
+        [SerializeField] Text _text;
+        [Header("Submit Event")]
+        [SerializeField] Button _submitBtn;
+        [SerializeField] UnityEvent _afterSubmitEvent;
 
         string GetUserInput()
         {
             if (_InputFieldTmp != null) return _InputFieldTmp.text;
-            else if (_formTextTmp != null) return _formTextTmp.text;
+            else if (_textTmp != null) return _textTmp.text;
             else if (_inputField != null) return _inputField.text;
-            else if (_formText != null) return _formText.text;
+            else if (_text != null) return _text.text;
             else
             {
                 Debug.Log($"<color=#4ec9b0>User Input Field ERROR</color>");
@@ -36,10 +42,42 @@ namespace Meangpu.Util
             }
         }
 
+        void ResetUserInput()
+        {
+            if (_InputFieldTmp != null) _InputFieldTmp.text = "";
+            else if (_textTmp != null) _textTmp.text = "";
+            else if (_inputField != null) _inputField.text = "";
+            else if (_text != null) _text.text = "";
+            else
+            {
+                Debug.Log($"<color=#4ec9b0>User Input Field ERROR</color>");
+            }
+        }
+
+
         [Button]
         public void SubmitUserFeedbackToForm()
         {
+            StopAllCoroutines();
+            SetSubmitBtnInteractable(false);
             StartCoroutine(PostUserFeedback(GetUserInput()));
+        }
+
+        [Button]
+        public void MakeFormCanSubmitAgain()
+        {
+            SetSubmitBtnInteractable(true);
+            ResetUserInput();
+        }
+
+        public void SetSubmitBtnInteractable(bool canInteract)
+        {
+            if (_submitBtn == null)
+            {
+                Debug.Log($"<color=#4ec9b0>No submit button!!</color>");
+                return;
+            }
+            _submitBtn.interactable = canInteract;
         }
 
         IEnumerator PostUserFeedback(string text)
@@ -47,19 +85,20 @@ namespace Meangpu.Util
             if (string.IsNullOrWhiteSpace(_formSubmitActionURL) || string.IsNullOrWhiteSpace(_formFieldURL))
             {
                 Debug.Log($"<color=red>User form URL is null!</color>");
-                yield return new();
+                yield break;
             }
             if (string.IsNullOrEmpty(text))
             {
                 Debug.Log($"<color=red>User text is empty!</color>");
-                yield return new();
+                yield break;
             }
 
             WWWForm form = new();
             form.AddField(_formFieldURL, text);
             UnityWebRequest submitReq = UnityWebRequest.Post(_formSubmitActionURL, form);
             yield return submitReq.SendWebRequest();
-            Debug.Log("Submit Request: " + submitReq.downloadHandler.text);
+            Debug.Log($"add this '{text}' to google form at '{_formFieldURL}'");
+            _afterSubmitEvent?.Invoke();
         }
     }
 
