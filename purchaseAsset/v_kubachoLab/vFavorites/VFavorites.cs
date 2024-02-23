1a3daf051c4379e4ec3dd6de84fa7000c70e5663
meangpu <51825872+meangpu@users.noreply.github.com>
2024-02-23 14:54:18 +0700

    update v fav

--- purchaseAsset/v_kubachoLab/vFavorites/VFavorites.cs
+++ purchaseAsset/v_kubachoLab/vFavorites/VFavorites.cs
@@ -544,7 +544,7 @@ namespace VFavorites
         }
 
 
-        public const string version = "1.0.8";
+        public const string version = "1.0.9";
 
     }
 }
--- purchaseAsset/v_kubachoLab/vFavorites/VFavoritesMenuItems.cs
+++ purchaseAsset/v_kubachoLab/vFavorites/VFavoritesMenuItems.cs
@@ -14,11 +14,12 @@ namespace VFavorites
         const string menuDir = "Tools/vFavorites/";
 
 
-        [MenuItem(menuDir + "Join our Discord", false, 101)]
+        [MenuItem(menuDir + "Upgrade to vFavorites 2", false, 101)]
+        static void dadsadsas() => Application.OpenURL("https://assetstore.unity.com/packages/slug/263643?aid=1100lGLBn&pubref=menuupgrade");
+
+        [MenuItem(menuDir + "Join our Discord", false, 102)]
         static void dadsas() => Application.OpenURL("https://discord.gg/4dG9KsbspG");
 
-        [MenuItem(menuDir + "Get the rest of our Editor Ehnancers with a discount", false, 102)]
-        static void dadsadsas() => Application.OpenURL("https://assetstore.unity.com/packages/tools/utilities/editor-enhancers-bundle-251318?aid=1100lGLBn&pubref=menu");
 
         [MenuItem(menuDir + "Disable vFavorites", false, 1001)]
         static void das() => ToggleDefineDisabledInScript(typeof(VFavorites));
