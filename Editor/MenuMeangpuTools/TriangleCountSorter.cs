4c339a7e2f1b7f75375370394c5113c3a661d04c
meangpu <51825872+meangpu@users.noreply.github.com>
2024-02-23 16:37:08 +0700

    Update TriangleCountSorter.cs

--- Editor/MenuMeangpuTools/TriangleCountSorter.cs
+++ Editor/MenuMeangpuTools/TriangleCountSorter.cs
@@ -9,18 +9,20 @@ namespace Meangpu
         [MenuItem("MeangpuTools/PolyCount Sorter", priority = 9999)]
         public static void SortObjectsByTriangleCount()
         {
-            MeshFilter[] meshFilters = FindObjectsOfType<MeshFilter>();
-            SkinnedMeshRenderer[] skinMesh = FindObjectsOfType<SkinnedMeshRenderer>();
+            MeshFilter[] _meshFilters = FindObjectsOfType<MeshFilter>();
+            SkinnedMeshRenderer[] _skinMesh = FindObjectsOfType<SkinnedMeshRenderer>();
 
-            var sortedObjects = meshFilters
-                .OrderByDescending(meshFilter => meshFilter.sharedMesh.triangles.Length / 3)
-                .Select(meshFilter => new { nowObject = meshFilter.gameObject, TriangleCount = meshFilter.sharedMesh.triangles.Length / 3 })
-                .ToList();
 
-            var sortedSkinMesh = skinMesh
+            var sortedObjects = _meshFilters
+                .Where(meshFilter => meshFilter != null && meshFilter.sharedMesh != null)
                 .OrderByDescending(meshFilter => meshFilter.sharedMesh.triangles.Length / 3)
-                .Select(meshFilter => new { nowObject = meshFilter.gameObject, TriangleCount = meshFilter.sharedMesh.triangles.Length / 3 })
-                .ToList();
+                .Select(meshFilter => new { nowObject = meshFilter.gameObject, TriangleCount = meshFilter.sharedMesh.triangles.Length / 3 });
+
+            var sortedSkinMesh = _skinMesh
+                .Where(skinMeshFilter => skinMeshFilter != null && skinMeshFilter.sharedMesh != null)
+                .OrderByDescending(skinMeshFilter => skinMeshFilter.sharedMesh.triangles.Length / 3)
+                .Select(skinMeshFilter => new { nowObject = skinMeshFilter.gameObject, TriangleCount = skinMeshFilter.sharedMesh.triangles.Length / 3 })
+                ;
 
             Debug.Log("======= MESH FILER =======");
             foreach (var obj in sortedObjects)
