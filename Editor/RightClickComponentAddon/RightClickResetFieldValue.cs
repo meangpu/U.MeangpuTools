edce6c700373eb0110d224da8fc83198cd73bdda
meangpu <51825872+meangpu@users.noreply.github.com>
2024-04-12 19:51:30 +0700

    add vector 3 helper

---
+++ Editor/RightClickComponentAddon/RightClickResetFieldValue.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace Meangpu
+{
+    // learn from [Quick Property Menus In Unity - YouTube](https://www.youtube.com/watch?v=hBrLsyLGaB4)
+    // use by right click vector 3 and set all to zero or one
+    public static class RightClickResetFieldValue
+    {
+        [InitializeOnLoadMethod] public static void Init() => EditorApplication.contextualPropertyMenu += OnPropertyContextMenu;
+
+        private static void OnPropertyContextMenu(GenericMenu menu, SerializedProperty property)
+        {
+            switch (property.propertyType)
+            {
+                case SerializedPropertyType.Vector3:
+                    menu.AddItem(new GUIContent("Zero"), false, () =>
+                    {
+                        property.vector3Value = Vector3.zero;
+                        property.serializedObject.ApplyModifiedProperties();
+                    });
+
+                    menu.AddItem(new GUIContent("One"), false, () =>
+                    {
+                        property.vector3Value = Vector3.one;
+                        property.serializedObject.ApplyModifiedProperties();
+                    });
+
+                    break;
+                case SerializedPropertyType.Vector2:
+                    menu.AddItem(new GUIContent("Zero"), false, () =>
+                    {
+                        property.vector2Value = Vector2.zero;
+                        property.serializedObject.ApplyModifiedProperties();
+                    });
+
+                    menu.AddItem(new GUIContent("One"), false, () =>
+                    {
+                        property.vector3Value = Vector2.one;
+                        property.serializedObject.ApplyModifiedProperties();
+                    });
+                    break;
+
+                case SerializedPropertyType.Quaternion:
+                    menu.AddItem(new GUIContent("RESET_ZERO"), false, () =>
+                    {
+                        property.quaternionValue = Quaternion.identity;
+                        property.serializedObject.ApplyModifiedProperties();
+                    });
+                    break;
+
+                default:
+                    return;
+
+
+            }
+
+
+
+        }
+    }
+}
