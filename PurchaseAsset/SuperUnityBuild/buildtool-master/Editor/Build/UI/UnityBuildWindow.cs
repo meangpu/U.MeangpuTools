347a0ee0e522aaefd3eeab0ab4935022b300a3eb
meangpu <51825872+meangpu@users.noreply.github.com>
2024-04-20 18:20:15 +0700

    disable icon

--- PurchaseAsset/SuperUnityBuild/buildtool-master/Editor/Build/UI/UnityBuildWindow.cs
+++ PurchaseAsset/SuperUnityBuild/buildtool-master/Editor/Build/UI/UnityBuildWindow.cs
@@ -44,8 +44,8 @@ namespace SuperUnityBuild.BuildTool
 
         protected void OnEnable()
         {
-            GUIContent icon = EditorGUIUtility.IconContent("Packages/com.github.superunitybuild.buildtool/Editor/Assets/Textures/icon.png");
-            GUIContent title = new GUIContent("SuperUnityBuild", icon.image);
+            // GUIContent icon = EditorGUIUtility.IconContent("Packages/com.github.superunitybuild.buildtool/Editor/Assets/Textures/icon.png");
+            GUIContent title = new GUIContent("SuperUnityBuild");
             titleContent = title;
 
             BuildNotificationList.instance.InitializeErrors();
