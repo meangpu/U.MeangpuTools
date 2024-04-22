d9020f9e19f1d29b66245c137c1e0bde397562ed
meangpu <51825872+meangpu@users.noreply.github.com>
2024-04-22 16:17:43 +0700

    Update TimeScaleSetter.cs

--- Runtime/Util/TimeControl/TimeScaleSetter.cs
+++ Runtime/Util/TimeControl/TimeScaleSetter.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using Ryan;
 using UnityEngine;
 
@@ -7,18 +8,42 @@ namespace Meangpu
     {
         [SerializeField] FloatReference _timeScale;
 
+        Sequence _sequence;
+
+        bool _isSlowing;
+        public bool IsSlowing => _isSlowing;
+
         private void Update() => UpdateTimeScale();
 
-        public void ResetTimeScale()
-        {
-            _timeScale.Variable.SetValue(1);
-            UpdateTimeScale();
-        }
+        public void ResetTimeScale() => SetTimeScale(1);
 
         private void UpdateTimeScale()
         {
             Time.timeScale = _timeScale;
             Time.fixedDeltaTime = _timeScale * Time.deltaTime;
+        }
+
+        public void SetTimeScale(float newValue)
+        {
+            _timeScale.Variable.SetValue(newValue);
+            _sequence.Kill();
+            _isSlowing = false;
+            UpdateTimeScale();
+        }
+
+        public void SlowTimeForSecond(float timeScale = .1f, float duration = 1f)
+        {
+            if (_isSlowing) return;
+            _isSlowing = true;
+
+            _sequence.Kill();
+
+            _timeScale.Variable.SetValue(timeScale);
+            _sequence = DOTween.Sequence();
+
+            _sequence.Append(DOTween.To(() => _timeScale, x => _timeScale.Variable.SetValue(x), 1, duration).SetEase(Ease.InQuad).SetUpdate(true));
+
+            _sequence.OnComplete(() => _isSlowing = false);
         }
     }
 }
