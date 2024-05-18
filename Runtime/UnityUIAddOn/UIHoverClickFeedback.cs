6962499a5e12150aa5ced3516cdfc30493f491a0
meangpu <51825872+meangpu@users.noreply.github.com>
2024-05-18 12:40:24 +0700

    add ui hover

---
+++ Runtime/UnityUIAddOn/UIHoverClickFeedback.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Meangpu.Audio;
+using DG.Tweening;
+
+namespace Meangpu
+{
+    public class UIHoverClickFeedback : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+    {
+        [SerializeField] SOSound _hoverSound;
+        [SerializeField] SOSound _clickSound;
+        [SerializeField] SOSound _exitSound;
+
+        Vector3 _defaultTransform;
+        [SerializeField] float _hoverTransform = 1.04f;
+        [SerializeField] float _animationTime = .1f;
+        [SerializeField] Ease _easeType = Ease.InOutSine;
+
+        private void Awake() => _defaultTransform = transform.localScale;
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            _clickSound?.PlayOneShot();
+            transform.DOScale(_defaultTransform, _animationTime).SetEase(_easeType);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            _hoverSound?.PlayOneShot();
+            transform.DOScale(_hoverTransform, _animationTime).SetEase(_easeType);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            _exitSound?.PlayOneShot();
+            transform.DOScale(_defaultTransform, _animationTime).SetEase(_easeType);
+        }
+    }
+}
