079445cb142dc28e1fc321e7df56db26d56e479d
meangpu <51825872+meangpu@users.noreply.github.com>
2024-05-25 22:19:47 +0700

    update readonly
    encounter better way from this repo [DawnosaurDev/platformer-movement](https://github.com/DawnosaurDev/platformer-movement)

--- Editor/PropertyDrawer/ReadOnly.cs
+++ Editor/PropertyDrawer/ReadOnly.cs
@@ -5,14 +5,19 @@ namespace Meangpu
 {
     // learn from this awesome guy on internet [Read-only fields? - Unity Forum](https://forum.unity.com/threads/read-only-fields.68976/)
 
+    /// <summary>
+    /// Allows you to add '[ReadOnly]' before a variable so that it is shown but not editable in the inspector.
+    /// Small but useful script, to make your inspectors look pretty and useful :D
+    /// </summary>
     [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
-    public class PrivateReadOnly : PropertyDrawer
+    public class ReadOnlyPropertyDrawer : PropertyDrawer
     {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             GUI.enabled = false;
-            EditorGUI.PropertyField(position, property, label, true);
+            EditorGUI.PropertyField(position, property, label);
             GUI.enabled = true;
         }
     }
+    // Learn More with this Tutorial: https://youtu.be/r3nwTGLHygI///
 }
