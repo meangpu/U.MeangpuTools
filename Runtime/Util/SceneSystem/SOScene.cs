18e644a9d3f1d8a78ac9da7a2b410fbc0bdd6993
meangpu <51825872+meangpu@users.noreply.github.com>
2024-05-31 20:53:49 +0700

    Revert "update me tools"
    This reverts commit 85c4ec194885a1f269be445d909151b7de798a41.

--- Runtime/Util/SceneSystem/SOScene.cs
+++ Runtime/Util/SceneSystem/SOScene.cs
@@ -4,8 +4,46 @@ using UnityEngine;
 
 namespace Meangpu
 {
-    public class SOScene : BaseSOScene
+    [CreateAssetMenu(fileName = "SOScene", menuName = "Meangpu/SOScene")]
+    public class SOScene : ScriptableObject
     {
+#if UNITY_EDITOR
+        public SceneReference SceneRefData;
+
+        private void OnValidate()
+        {
+            if (SceneRefData == null) return;
+            if (SceneRefData.GetSceneName == null) return;
+            SCENE_ID = SceneRefData.GetSceneName;
+            UnityEditor.EditorUtility.SetDirty(this);
+        }
+
+        [Button]
+        public void RenameThisSOTOSceneName()
+        {
+            if (SceneRefData == null)
+            {
+                Debug.LogError("Scene data is null");
+                return;
+            }
+            string assetPath = UnityEditor.AssetDatabase.GetAssetPath(this.GetInstanceID());
+            UnityEditor.AssetDatabase.RenameAsset(assetPath, SceneRefData.GetSceneName);
+            UnityEditor.AssetDatabase.SaveAssets();
+            UnityEditor.EditorUtility.SetDirty(this);
+        }
+#endif
+
+        [ReadOnly] public string SCENE_ID;
+
+        public string SceneNameDisplay;
+        [TextArea]
+        public string SceneDescriptionShort;
+        [TextArea]
+        public string SceneDescription;
+        public Sprite Icon;
+        public Sprite ScenePreviewImage;
+        public bool IsDone;
+
         [Button]
         public virtual void LoadThisScene()
         {
