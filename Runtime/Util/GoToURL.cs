966270a14af5598bfe45eaf2bda2efe5edd4b278
meangpu <51825872+meangpu@users.noreply.github.com>
2024-06-01 15:37:18 +0700

    Update GoToURL.cs

--- Runtime/Util/GoToURL.cs
+++ Runtime/Util/GoToURL.cs
@@ -6,7 +6,18 @@ namespace Meangpu.Util
     public class GoToURL : MonoBehaviour
     {
         [SerializeField] string _linkToGo;
-        [Button] public void LoadThisScriptURL() => OpenLinkURL.OpenLink(_linkToGo);
-        [Button] public void LoadURL(string url) => OpenLinkURL.OpenLink(url);
+        [Button]
+        public void LoadThisScriptURL()
+        {
+            if (string.IsNullOrEmpty(_linkToGo)) return;
+            OpenLinkURL.OpenLink(_linkToGo);
+        }
+
+        [Button]
+        public void LoadURL(string url)
+        {
+            if (string.IsNullOrEmpty(_linkToGo)) return;
+            OpenLinkURL.OpenLink(url);
+        }
     }
 }
