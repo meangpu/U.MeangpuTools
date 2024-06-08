79e60d20652745887c6303a1d8583c7b464558c2
meangpu <51825872+meangpu@users.noreply.github.com>
2024-06-09 02:04:48 +0700

    Update Billboard.cs

--- Runtime/Util/Billboard.cs
+++ Runtime/Util/Billboard.cs
@@ -4,13 +4,37 @@ namespace Meangpu.Util
 {
     public class Billboard : MonoBehaviour
     {
-        Camera mainCamera;
-        void Start() => mainCamera = Camera.main;
+        private enum Mode
+        {
+            LookAt,
+            LookAtInverted,
+            CameraForward,
+            CameraForwardInverted,
+        }
+
+        [SerializeField] Mode _cameraMode = Mode.CameraForward;
+        Camera _mainCamera;
+
+        void Start() => _mainCamera = Camera.main;
 
         void LateUpdate()
         {
-            transform.LookAt(mainCamera.transform);
-            transform.Rotate(0, 180, 0);
+            switch (_cameraMode)
+            {
+                case Mode.LookAt:
+                    transform.LookAt(_mainCamera.transform);
+                    break;
+                case Mode.LookAtInverted:
+                    Vector3 dirFromCamera = transform.position - _mainCamera.transform.position;
+                    transform.LookAt(transform.position + dirFromCamera);
+                    break;
+                case Mode.CameraForward:
+                    transform.forward = _mainCamera.transform.forward;
+                    break;
+                case Mode.CameraForwardInverted:
+                    transform.forward = -_mainCamera.transform.forward;
+                    break;
+            }
         }
     }
 }
