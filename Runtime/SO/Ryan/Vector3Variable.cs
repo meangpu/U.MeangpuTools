12671b07e2e2a9fdab85e45e766e69b69472ed4c
meangpu <51825872+meangpu@users.noreply.github.com>
2024-06-10 07:43:43 +0700

    Update Vector3Variable.cs

--- Runtime/SO/Ryan/Vector3Variable.cs
+++ Runtime/SO/Ryan/Vector3Variable.cs
@@ -15,6 +15,7 @@ namespace Meangpu
         public void SetValue(Vector3Variable value) => Value = value.Value;
         public void ApplyChange(Vector3 amount) => Value += amount;
         public void ApplyChange(Vector3Variable amount) => Value += amount.Value;
+        public void SetValueToXZ(Vector2 value) => Value = new(value.x, 0, value.y);
     }
 }
 
